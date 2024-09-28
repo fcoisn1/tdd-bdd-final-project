08031d1d490c879451d299d1011ce460be0b484b
frank <francisco_isn@hotmail.com>
2024-09-27 22:36:04 -0600

    test update issue

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -182,15 +182,15 @@ class TestProductRoutes(TestCase):
     def test_update_product(self):
         """It should Update an existing Product"""
         test_product = ProductFactory()
-        response = self.client.port(BASE_URL, json=test_product.serialize())
+        response = self.client.post(BASE_URL, json=test_product.serialize())
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    #     new_product = response.get_json()
-    #     new_product["description"] = "unknown"
-    #     response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     updated_product = response.get_json()
-    #     self.assertEqual(updated_product["description"], "unknown")
+        new_product = response.get_json()
+        new_product["description"] = "unknown"
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_product = response.get_json()
+        self.assertEqual(updated_product["description"], "unknown")
 
     # def test_delete_product(self):
     #     """It should Delete a Product"""
