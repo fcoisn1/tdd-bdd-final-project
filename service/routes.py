e72ea2d69f7c6563c5e03c7fef22d1f5829425ef
frank <francisco_isn@hotmail.com>
2024-09-27 23:14:39 -0600

    route.py list of products

--- service/routes.py
+++ service/routes.py
@@ -170,32 +170,32 @@ def update_products(product_id):
 ##############################################################################
 # LIST PRODUCTS
 ##############################################################################
-# @app.route("/products", methods = ["GET"])
-# def list_products():
-#     """Returns a list of Products"""
-#     products = []
-#     name = request.args.get("name")
-#     category = request.args.get("category")
-#     available = request.args.get("available")
+@app.route("/products", methods = ["GET"])
+def list_products():
+    """Returns a list of Products"""
+    products = []
+    name = request.args.get("name")
+    category = request.args.get("category")
+    available = request.args.get("available")
 
-#     if name:
-#         app.logger.info("Find by name: %s", name)
-#         products = Product.find_by_name(name)
-#     elif category:
-#         app.logger.info("Find by category: %s", category)
-#         category_value = getattr(Category, category.upper())
-#         products = Product.find_by_category(category_value)
-#     elif available:
-#         app.logger.info("Find by available:%s", available)
-#         available_value = available.lower() in ["true", "yes", "1"]
-#         products = Product.find_by_availability(available_value)
-#     else:
-#         app.logger.info("Find all")
-#         products = Product.all()
+    if name:
+        app.logger.info("Find by name: %s", name)
+        products = Product.find_by_name(name)
+    elif category:
+        app.logger.info("Find by category: %s", category)
+        category_value = getattr(Category, category.upper())
+        products = Product.find_by_category(category_value)
+    elif available:
+        app.logger.info("Find by available:%s", available)
+        available_value = available.lower() in ["true", "yes", "1"]
+        products = Product.find_by_availability(available_value)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
     
-#     results = [product.serialize() for product in products]
-#     app.logger.info("[%s] Products returned", len(results))
-#     return results, status.HTTP_200_OK
+    results = [product.serialize() for product in products]
+    app.logger.info("[%s] Products returned", len(results))
+    return results, status.HTTP_200_OK
 
 
         
