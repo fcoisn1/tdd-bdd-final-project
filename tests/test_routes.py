18db835af695d690a397bc486a736877e4e1a892
frank <francisco_isn@hotmail.com>
2024-09-27 18:44:40 -0600

    fix issues test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -171,7 +171,7 @@ class TestProductRoutes(TestCase):
         response = self.client.get(f"{BASE_URL}/{test_product.id}")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
-        self.asserEqual(data["name"], test_product.name)
+        self.assertEqual(data["name"], test_product.name)
     def test_get_product_not_found(self):
         """It should get product that was not found"""
         response = self.client.get(f"{BASE_URL}/0")
