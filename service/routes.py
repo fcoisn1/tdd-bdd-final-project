fde893d56ea4f0bcfc6cbf9a456d528a33fdf96a
frank <francisco_isn@hotmail.com>
2024-09-27 22:32:00 -0600

    app.route def update product

--- service/routes.py
+++ service/routes.py
@@ -151,21 +151,21 @@ def get_products(product_id):
 # UPDTE AN EXISTING PRODUCT
 #######################################################################
 # @app.route("/products/<int:product_id>", methods=["PUT"])
-# def update_products(product_id):
-#     """
-#     Update a Product
-#     This endpoint will update a Product based the body that is posted
-#     """
-#     app.logger.info("Request to Update a product with id [%s]", product_id)
-#     check_content_type("application/json")
+def update_products(product_id):
+    """
+    Update a Product
+    This endpoint will update a Product based the body that is posted
+    """
+    app.logger.info("Request to Update a product with id [%s]", product_id)
+    check_content_type("application/json")
 
-#     product = Product.find(product_id)
-#     if not product:
-#         abort(status.HTTP_404_NOT_FOUND, f"Product with id'{product_id}' was not found")
-#     product.deserialize(request.get_json)
-#     product.id = product_id
-#     product.update()
-#     return product.serialize(), status.HTTP_200_OK
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id'{product_id}' was not found")
+    product.deserialize(request.get_json)
+    product.id = product_id
+    product.update()
+    return product.serialize(), status.HTTP_200_OK
 
 ##############################################################################
 # LIST PRODUCTS
